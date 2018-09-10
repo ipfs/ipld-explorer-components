3af7555408dd6c330fba115f78de1526584306ec
Marcin Rataj <lidel@lidel.org>
2018-09-10 17:42:38 +0200

    feat(i18n): pl_PL
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/i18n.js
+++ src/i18n.js
@@ -4,10 +4,11 @@ import XHR from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from 'i18next-icu/locale-data/en'
+import pl from 'i18next-icu/locale-data/pl'
 
 i18n
   .use(new ICU({
-    localeData: [en]
+    localeData: [en, pl]
   }))
   .use(XHR)
   .use(LanguageDetector)
