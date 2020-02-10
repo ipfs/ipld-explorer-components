415e8e90d3541f4f0501b34543a298430b213175
Marcin Rataj <lidel@lidel.org>
2020-02-11 00:17:18 +0100

    chore(i18n): sync translations with Transifex (#213)
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/i18n.js
+++ src/i18n.js
@@ -3,13 +3,25 @@ import ICU from 'i18next-icu'
 import XHR from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
+import cs from 'i18next-icu/locale-data/cs'
+import da from 'i18next-icu/locale-data/da'
+import de from 'i18next-icu/locale-data/de'
 import en from 'i18next-icu/locale-data/en'
+import es from 'i18next-icu/locale-data/es'
+import fr from 'i18next-icu/locale-data/fr'
+import it from 'i18next-icu/locale-data/it'
+import ja from 'i18next-icu/locale-data/ja'
+import ko from 'i18next-icu/locale-data/ko'
+import nl from 'i18next-icu/locale-data/nl'
+import no from 'i18next-icu/locale-data/no'
 import pl from 'i18next-icu/locale-data/pl'
 import pt from 'i18next-icu/locale-data/pt'
+import sv from 'i18next-icu/locale-data/sv'
+import zh from 'i18next-icu/locale-data/zh'
 
 i18n
   .use(new ICU({
-    localeData: [en, pl, pt]
+    localeData: [cs, da, de, en, es, fr, it, ja, ko, nl, no, pl, pt, sv, zh]
   }))
   .use(XHR)
   .use(LanguageDetector)
