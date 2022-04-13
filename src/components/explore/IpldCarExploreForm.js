cd010c9543c6829d34e647701e6c7c97b76562cb
alvin-reyes <me@areyes.onl>
2022-04-13 16:29:19 -0400

    chore: added condition to only accept .car files

--- src/components/explore/IpldCarExploreForm.js
+++ src/components/explore/IpldCarExploreForm.js
@@ -30,7 +30,7 @@ class IpldCarExploreForm extends React.Component {
       <form data-id='IpldCarExploreForm' className='sans-serif black-80 flex' onSubmit={this.handleOnSubmit} encType='multipart/form-data'>
         <div className='flex-auto'>
           <div className='relative'>
-            <input id='car-file' type='file' className='input-reset bn pa2 mb2 db w-100 f6 br-0 placeholder-light focus-outline' style={{ borderRadius: '3px 0 0 3px', backgroundColor: 'white', padding: '5px 0px 5px 5px', width: '99%' }} aria-describedby='name-desc' onChange={this.handleOnChange} />
+            <input id='car-file' type='file' accept='.car' className='input-reset bn pa2 mb2 db w-100 f6 br-0 placeholder-light focus-outline' style={{ borderRadius: '3px 0 0 3px', backgroundColor: 'white', padding: '5px 0px 5px 5px', width: '99%' }} aria-describedby='name-desc' onChange={this.handleOnChange} />
             <small id='car-file-desc' className='o-0 absolute f6 black-60 db mb2'>{t('IpldCarExploreForm.uploadCarFile')}</small>
           </div>
         </div>
