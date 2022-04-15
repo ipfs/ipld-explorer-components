798dda59fe56964b8582241608a7cec6812db800
alvin-reyes <me@areyes.onl>
2022-04-15 18:27:24 -0400

    chore: fixed lint issues

--- src/components/explore/IpldCarExploreForm.js
+++ src/components/explore/IpldCarExploreForm.js
@@ -7,7 +7,7 @@ class IpldCarExploreForm extends React.Component {
     super(props)
     this.state = {
       file: { },
-      uploadIcon: this.props.uploadIcon,
+      uploadIcon: this.props.uploadIcon
     }
     this.handleOnChange = this.handleOnChange.bind(this)
     this.handleOnSubmit = this.handleOnSubmit.bind(this)
@@ -18,19 +18,15 @@ class IpldCarExploreForm extends React.Component {
   }
 
   handleOnChange () {
-
     this.setState({ uploadIcon: this.props.spinnerIcon }, () => {
-
       //  Change the state.
-      let imageFileLoader = document.getElementById('car-loader-image')
+      const imageFileLoader = document.getElementById('car-loader-image')
       imageFileLoader.src = this.props.spinnerIcon
 
       const selectedFile = document.getElementById('car-file').files[0]
       this.setState({ file: selectedFile })
       this.props.doUploadUserProvidedCar(selectedFile, this.props.uploadIcon)
-
     })
-
   }
 
   render () {
@@ -41,7 +37,7 @@ class IpldCarExploreForm extends React.Component {
           <div className='relative'>
             <input id='car-file' type='file' accept='.car' className='input-reset bn pa2 mb2 db w-100 f6 br-0 placeholder-light focus-outline' style={{ borderRadius: '3px 0 0 3px', backgroundColor: 'white', padding: '5px 0px 5px 5px', width: '99%' }} aria-describedby='name-desc' onChange={this.handleOnChange} />
             <small id='car-file-desc' className='o-0 absolute f6 black-60 db mb2'>{t('IpldCarExploreForm.uploadCarFile')}</small>
-            <img id='car-loader-image' alt='placeholder for upload and loader image' src={this.state.uploadIcon} className='absolute' style={{ top: '0px', right: '0px', height: '30px', width: '30px', paddingRight: '10px' }} />
+            <img id='car-loader-image' alt='placeholder for upload and loader' src={this.state.uploadIcon} className='absolute' style={{ top: '0px', right: '0px', height: '30px', width: '30px', paddingRight: '10px' }} />
           </div>
         </div>
       </form>
