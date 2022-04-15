c7df739f0e1d8b9cd82871d63532fb486dd769c1
alvin-reyes <me@areyes.onl>
2022-04-15 18:18:38 -0400

    chore: added alt on img

--- src/components/explore/IpldCarExploreForm.js
+++ src/components/explore/IpldCarExploreForm.js
@@ -41,7 +41,7 @@ class IpldCarExploreForm extends React.Component {
           <div className='relative'>
             <input id='car-file' type='file' accept='.car' className='input-reset bn pa2 mb2 db w-100 f6 br-0 placeholder-light focus-outline' style={{ borderRadius: '3px 0 0 3px', backgroundColor: 'white', padding: '5px 0px 5px 5px', width: '99%' }} aria-describedby='name-desc' onChange={this.handleOnChange} />
             <small id='car-file-desc' className='o-0 absolute f6 black-60 db mb2'>{t('IpldCarExploreForm.uploadCarFile')}</small>
-            <img id='car-loader-image' src={this.state.uploadIcon} className='absolute' style={{ top: '0px', right: '0px', height: '30px', width: '30px', paddingRight: '10px' }} />
+            <img id='car-loader-image' alt='placeholder for upload and loader image' src={this.state.uploadIcon} className='absolute' style={{ top: '0px', right: '0px', height: '30px', width: '30px', paddingRight: '10px' }} />
           </div>
         </div>
       </form>
