f6f1b533c7c36452d308e39f72f4d80d43182983
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-05-24 13:59:30 -0700

    fix: can consume with ipld.explore.io

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,91 +9,53 @@ import postcss from 'rollup-plugin-postcss'
 import json from '@rollup/plugin-json';
 import svg from 'rollup-plugin-svg'
 import { babel } from '@rollup/plugin-babel';
-console.log(`babel: `, babel);
+import copy from 'rollup-plugin-copy'
+import autoExternal from 'rollup-plugin-auto-external';
 
 import packageJson from './package.json' assert { type: 'json' }
 
-import { basename, dirname, isAbsolute } from 'path';
-import * as fs from 'fs';
-
-function isFile ( file ) {
-    try {
-        return fs.statSync( file ).isFile();
-    } catch ( err ) {
-        return false;
-    }
-}
-
-function addExtensionIfNecessary ( file, extensions ) {
-    try {
-        const name = basename( file );
-        const files = fs.readdirSync( dirname( file ) );
-
-        if ( ~files.indexOf( name ) && isFile( file ) ) return file;
-        for ( const ext of extensions ) {
-            if ( ~files.indexOf( `${name}${ext}` ) && isFile( `${file}${ext}` ) ) {
-                return `${file}${ext}`;
-            }
-        }
-    } catch ( err ) {
-        // noop
-    }
-
-    return null;
-}
-
-function extensions ({extensions}) {
-    if (!extensions || !extensions.length) {
-        throw new Error( `Must specify { extensions: [..] } as non-empty array!` );
-    }
-
-    return {
-        name: 'extensions',
-
-        resolveId ( importee, importer ) {
-            // absolute paths are left untouched
-            if ( isAbsolute( importee ) ) {
-                return addExtensionIfNecessary( resolve( importee ), extensions );
-            }
-
-            // if this is the entry point, resolve against cwd
-            if ( importer === undefined ) {
-                return addExtensionIfNecessary( resolve( process.cwd(), importee ), extensions );
-            }
-
-            // external modules are skipped at this stage
-            if ( importee[0] !== '.' ) return null;
-
-            return addExtensionIfNecessary( resolve( dirname( importer ), importee ), extensions );
-        }
-    };
-}
-
+/** @type {import('rollup').NormalizedInputOptions[]} */
 export default [
   {
-    input: 'dist/index.js',
+    input: 'dist-esm/index.js',
     // dir: 'dist',
     output: [
       {
         // file: packageJson.module,
-        dir: 'dist/cjs',
+        dir: 'dist-cjs',
         format: 'cjs',
-        sourcemap: true
+        sourcemap: true,
+        // preserveModules: true,
+        // exports: 'named',
       }
     ],
     // acornInjectPlugins: [jsx()],
 
     plugins: [
+      // autoExternal({
+      //   builtins: true,
+      //   dependencies: false,
+      //   peerDependencies: true,
+      // }),
+      copy({
+        targets: [
+          // { src: 'dist-esm/**/*.css', dest: 'dist-cjs' },
+          {
+            src: 'dist-esm/**/*.svg', dest: 'dist-cjs', flatten: false,
+            // rename: (name, extension, fullPath) => `${fullPath.replace('dist-esm', '')}`
+          }
+        ]
+      }),
+      resolve(),
       postcss({
-        include: "dist/components/object-info/LinksTable.css",
-        extract: resolve('dist/cjs/components/object-info/LinksTable.css')
+        include: "dist-esm/components/object-info/LinksTable.css",
+        extract: resolve('dist-cjs/components/object-info/LinksTable.css')
       }),
       postcss({
-        include: "dist/components/loader/Loader.css",
-        extract: resolve('dist/cjs/components/loader/Loader.css')
+        include: "dist-esm/components/loader/Loader.css",
+        extract: resolve('dist-cjs/components/loader/Loader.css')
       }),
-      resolve({extensions: ['.js', '.jsx', '.ts', '.tsx']}),
-      babel({ babelHelpers: 'bundled', extensions: [".js", ".jsx"] }),
+      // babel({ babelHelpers: 'bundled', extensions: [".js", ".jsx"] }),
       // extensions({
       //   extensions: [ '.jsx', '.js'],
       // }),
@@ -105,14 +67,15 @@ export default [
       json(),
       svg(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.rollup.json' }),
+      // typescript({ tsconfig: './tsconfig.rollup.json' }),
       // terser(),
     ],
-    external: ['react', 'react-dom', 'styled-components']
+    external: ['react', 'react-dom']
+    // external: Object.keys(packageJson.dependencies || {}).concat(Object.keys(packageJson.peerDependencies || {})).concat(Object.keys(packageJson.devDependencies || {}))
   },
-  // {
-  //   input: 'src/index.ts',
-  //   output: [{ file: 'dist/cjs/types.d.ts', format: 'es' }],
-  //   plugins: [dts.default()]
-  // }
+  {
+    input: 'dist-esm/index.d.ts',
+    output: [{ file: 'dist-cjs/index.d.ts', format: 'cjs' }],
+    plugins: [dts.default()]
+  }
 ]
