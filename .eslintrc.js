36fd80a6fe00c3222ce3e50f6b6ee73f96d4ad83
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-05-26 15:05:03 -0700

    fix: lint runs (currently failing)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,28 @@
 module.exports = {
-    parser: 'babel-eslint',
-    extends: ['react-app', 'standard'],
-    plugins: ['jsx-a11y'],
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'react-app',
+      'standard',
+      'ipfs',
+    ],
+    plugins: [
+      'import',
+      '@typescript-eslint',
+      'jsx-a11y',
+    ],
+    rules: {
+      'import/order': ['error', {
+        groups: [
+          'builtin', // Built-in types are first
+          ['external', 'unknown'],
+          ['parent', 'sibling', 'internal', 'index']
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        },
+        warnOnUnassignedImports: true
+      }],
+  },
 }
