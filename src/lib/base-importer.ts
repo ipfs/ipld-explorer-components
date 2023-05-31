46a03ca322457596f386ebeb1671dc46b7bad9de
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-05-31 14:25:04 -0700

    fix: improve base-importer

--- src/lib/base-importer.ts
+++ src/lib/base-importer.ts
@@ -1,17 +1,20 @@
 import { bases } from 'multiformats/basics'
 
-export default async function baseImporter<T extends string> (prefix: T) {
-  console.log('bases: ', bases)
-  for (const base in bases) {
-    // @ts-expect-error
-    const multibase = bases[base]
-    if (multibase.prefix === prefix) {
-      return multibase
-    }
-  }
+export default async function baseImporter<T extends string> (prefix: T): Promise<typeof bases[keyof typeof bases]> {
   // handle for CIDv0
   if (prefix === 'Q') {
     return bases.base58btc
+  }
+
+  const base = Object.values(bases).find((base) => {
+    if (base.prefix === prefix) {
+      return true
+    }
+    return false
+  })
+
+  if (base != null) {
+    return base
   }
 
   throw new Error(`unknown multibase prefix '${prefix}'`)
