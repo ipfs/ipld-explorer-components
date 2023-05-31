ad730de3ec93576fb32aa32632f00c88665f28c5
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-05-31 14:16:21 -0700

    fix: consolidate decodeCid function

--- src/components/cid-info/decode-cid.js
+++ src/components/cid-info/decode-cid.js
@@ -5,53 +5,28 @@ import hashImporter from '../../lib/hash-importer.js'
 
 export async function decodeCid (value) {
   const cid = CID.parse(value)
-  // console.log(`cid: `, cid);
-  if (cid.version === 0) {
-    return await decodeCidV0(value, cid)
-  }
-  if (cid.version === 1) {
-    return await decodeCidV1(value, cid)
-  }
-  throw new Error('Unknown CID version', cid.version, cid)
-}
-
-// cidv0 ::= <multihash-content-address>
-// QmRds34t1KFiatDY6yJFj8U9VPTLvSMsR63y7qdUV3RMmT
-export async function decodeCidV0 (value, cid) {
-  const multihasher = await hashImporter(cid.multihash.code)
-
-  const multibase = await baseImporter(value.substring(0, 1))
-  return {
-    cid,
-    multibase,
-    multicodec: {
-      name: cid.code,
-      code: 'implicit'
-    },
-    multihash: {
-      name: multihasher.name,
-      ...cid.multihash
+  try {
+    const multihasher = await hashImporter(cid.multihash.code)
+    const multibase = await baseImporter(value.substring(0, 1))
+    const multicodec = {
+      name: cid.code
     }
-  }
-}
-
-// <cidv1> ::= <multibase-prefix><cid-version><multicodec-content-type><multihash-content-address>
-// zb2rhiVd5G2DSpnbYtty8NhYHeDvNkPxjSqA7YbDPuhdihj9L
-export async function decodeCidV1 (value, cid) {
-  const multihasher = await hashImporter(cid.multihash.code)
-  const multibase = await baseImporter(value.substring(0, 1))
-
-  console.log('cid.code.toString(16): ', cid.code.toString(16))
-  return {
-    cid,
-    multibase,
-    multicodec: {
-      name: cid.code,
-      code: '0x' + cid.code.toString(16)
-    },
-    multihash: {
-      name: multihasher.name,
-      ...cid.multihash
+    if (cid.version === 0) {
+      multicodec.code = 'implicit'
+    } else if (cid.version >= 1) {
+      multicodec.code = `0x${cid.code.toString(16)}`
     }
+
+    return {
+      cid,
+      multibase,
+      multicodec,
+      multihash: {
+        name: multihasher.name,
+        ...cid.multihash
+      }
+    }
+  } catch (err) {
+    console.error('Failed to decode CID', err)
   }
 }
