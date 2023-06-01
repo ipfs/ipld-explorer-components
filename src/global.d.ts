72a6e818ffe3a51523c6829ea22e627f6812fec9
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-06-01 13:50:11 -0700

    fix: add stream-to-it types

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @see https://www.npmjs.com/package/stream-to-it#api
+ */
+declare module 'stream-to-it' {
+  interface toIterable {
+    source: <T>(stream: ReadableStream<T>) => AsyncIterable<T>
+  }
+  const toIterable: toIterable
+  export default toIterable
+}
