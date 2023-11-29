57fe72500d3f9270d12bc772634f96616013b1f9
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-11-28 17:27:13 -0800

    chore: fix lint after eslint-config-ipfs update

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -11,7 +11,7 @@ export interface DigestFn {
   (data: Uint8Array): Promise<MultihashDigest<number>>
 }
 
-export async function addDagNodeToHelia <T> (helia: Helia, codec: { encode: (n: T) => Uint8Array, code: number }, node: T, digestFn?: DigestFn): Promise<CID> {
+export async function addDagNodeToHelia <T> (helia: Helia, codec: { encode(n: T): Uint8Array, code: number }, node: T, digestFn?: DigestFn): Promise<CID> {
   const encodedNode = codec.encode(node)
   const hash = digestFn != null ? await digestFn(encodedNode) : await sha256.digest(encodedNode)
   const cid = CID.createV1(codec.code, hash)
