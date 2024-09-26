44c16281d9841e20c13bc931e32838bd177f846b
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-09-25 21:52:46 -0500

    chore: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,20 +5,32 @@ import viteConfig from '../vite.config';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    // '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-coverage'
+    '@storybook/addon-coverage',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  typescript: { reactDocgen: 'react-docgen-typescript' },
+
   // async viteFinal(config) {
   //   // Merge custom configuration into the default config
   //   return mergeConfig(config, viteConfig);
   // },
+  typescript: {
+    // reactDocgen: 'react-docgen-typescript'
+    reactDocgen: false
+    // reactDocgenTypescriptOptions: {
+
+    // }
+  },
+
+  // docs: {}
 };
 export default config;
