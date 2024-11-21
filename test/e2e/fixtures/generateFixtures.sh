62aded41d5bb39741cbc9cd0a18518752b26e555
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-11-21 16:30:07 -0600

    chore: generate-fixtures-script allows paths

--- test/e2e/fixtures/generateFixtures.sh
+++ test/e2e/fixtures/generateFixtures.sh
@@ -11,6 +11,31 @@ save_fixture() {
   npx kubo block get $fixture_cid > $fixture_path
 }
 
+# If a user requests to generate all the fixtures for a full path, we need to loop through the path and save each block
+handlePath() {
+  local path=$1
+  shift
+  local save_path=$1
+  shift
+
+  # while the path contains slashes, we need to find the CID at each part of the path, save the block, and remove the trailing part in the path
+  while (( $(echo $path | grep -o '/' | wc -l) > 0 )); do
+    echo "Path=$path"
+    local cid_for_path_part=$(npx kubo block stat $path | ggrep '^Key:' | awk -F': ' '{print $2}')
+
+    echo "CID=$cid_for_path_part"
+    # save the block
+    save_fixture $cid_for_path_part $save_path
+    # remove the right-most part from the path
+    path=$(echo $path | rev | cut -d'/' -f2- | rev)
+  done
+
+  echo "Path=$path"
+  echo "CID=$path"
+  # now get the last CID in the path and save it
+  save_fixture $path $save_path
+}
+
 # Example call:
 # test/e2e/fixtures/generateFixtures.sh QmSnuWmxptJZdLJpKRarxBMS2Ju2oANVrgbr2xWbie9b2D explore/blocks
 # saves passed root_cid block to the passed save_path
@@ -38,15 +63,18 @@ main () {
   echo -e "\$DIR: $DIR \n"
   local root_cid=$1
   shift
-  local save_path="$DIR/$1"
-  shift
+  local save_path="$DIR/explore/blocks"
 
-  echo -e "\$root_cid: $root_cid \n"
-
-  save_fixture $root_cid $save_path
+  # if instead of an individual CID, we get a path, we need to loop through the path and save each block
+  if [[ $root_cid == *"/"* ]]; then
+    handlePath $root_cid $save_path
+  else
+    echo -e "\$root_cid: $root_cid \n"
+    save_fixture $root_cid $save_path
+  fi
 
   # Children are not needed for now, but you can get them like so:
-  # for cid in $(ipfs ls $root_cid | awk '{ print $1}'); do
+  # for cid in $(npx kubo ls $root_cid | awk '{ print $1}'); do
   #   echo -e "\$cid: $cid \n"
   #   save_fixture $cid $save_path
   # done
