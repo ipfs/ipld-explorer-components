5cbde309db2707c80c967d9eb3e2ff064ffd0671
george-hub331 <idiaghegeorge9@gmail.com>
2025-02-17 12:47:17 +0100

    feat: add link to check.ipfs.network on block fetch failure

--- src/components/explore/IpldExploreErrorComponent.tsx
+++ src/components/explore/IpldExploreErrorComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { BlockFetchTimeoutError } from '../../lib/errors'
 import type IpldExploreError from '../../lib/errors'
 
 export interface IpldExploreErrorComponentProps {
@@ -10,9 +11,23 @@ export function IpldExploreErrorComponent ({ error }: IpldExploreErrorComponentP
   const { t } = useTranslation('explore', { keyPrefix: 'errors' })
   if (error == null) return null
 
+  const isBlockError = error instanceof BlockFetchTimeoutError
+
   return (
     <div className='bg-red white pa3 lh-copy'>
       <div>{error.toString(t)}</div>
+      {isBlockError && (
+        <div className='mt2'>
+          <a
+            href={`https://check.ipfs.network/#/ipfs/${error.cid ?? ''}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className='white underline hover-white'
+          >
+            {t('checkIpfsNetwork')}
+          </a>
+        </div>
+      )}
     </div>
   )
 }
