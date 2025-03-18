83099425d309cc656ec55600318358285348e28d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-03-18 07:50:54 -0500

    fix: center IpldExploreErrorComponent

--- src/components/explore/IpldExploreErrorComponent.tsx
+++ src/components/explore/IpldExploreErrorComponent.tsx
@@ -7,6 +7,35 @@ export interface IpldExploreErrorComponentProps {
   error: IpldExploreError | null
 }
 
+const TroubleshootingTips: React.FC<{ cid: string }> = ({ cid }) => {
+  const { t } = useTranslation('explore', { keyPrefix: 'errors' })
+  if (cid == null) return null
+
+  return (
+    <>
+      <div className='mt3'>
+        <h4 className='ma0 mb2'>{t('troubleshootingTips.title')}</h4>
+        <ul className='ma0 pl3'>
+          <li>{t('troubleshootingTips.refresh')}</li>
+          <li>{t('troubleshootingTips.checkConnection')}</li>
+          <li>{t('troubleshootingTips.tryLater')}</li>
+          <li>{t('troubleshootingTips.checkCidSyntax')}</li>
+        </ul>
+      </div>
+      <div className='mt2'>
+        <a
+          href={`https://check.ipfs.network/?cid=${cid}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className='red-dark hover-red underline'
+        >
+          {t('checkIpfsNetwork')}
+        </a>
+    </div>
+    </>
+  )
+}
+
 export function IpldExploreErrorComponent ({ error }: IpldExploreErrorComponentProps): JSX.Element | null {
   const { exploreState } = useExplore()
   const { path } = exploreState
@@ -16,28 +45,11 @@ export function IpldExploreErrorComponent ({ error }: IpldExploreErrorComponentP
 
   // more self service
   return (
-    <div className='bg-red white pa3 lh-copy'>
-      <div>{error.toString(t)}</div>
-      {cid != null && <><div className='mt3'>
-        <h4 className='ma0 mb2'>{t('troubleshootingTips.title')}</h4>
-        <ul className='ma0 pl3'>
-          <li>{t('troubleshootingTips.refresh')}</li>
-          <li>{t('troubleshootingTips.checkConnection')}</li>
-          <li>{t('troubleshootingTips.tryLater')}</li>
-          <li>{t('troubleshootingTips.checkCidSyntax')}</li>
-        </ul>
+    <div className="flex justify-center w-100 pa3">
+      <div className="bg-red-muted red-dark pa3 br2 lh-copy mw7">
+        <div>{error.toString(t)}</div>
+        <TroubleshootingTips cid={cid} />
       </div>
-        <div className='mt2'>
-          <a
-            href={`https://check.ipfs.network/?cid=${cid}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className='white underline hover-white'
-          >
-            {t('checkIpfsNetwork')}
-          </a>
-        </div>
-      </>}
     </div>
   )
 }
